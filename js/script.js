0a1fda8dda1882fe840a72d56bed4c4875735e61
Igor Czyz <igorczyz13@gmail.com>
2020-10-22 18:49:33 +0200

    Add game logic

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
-​
 console.log('Wylosowana liczba to: ' + randomNumber);
-​
 let computerMove = 'nieznany ruch';
-​
 if(randomNumber == 1){
   computerMove = 'kamień';
 }
 else if(randomNumber == 2){
     computerMove = 'papier';
 }
-else if(randomNumer == 3){
+else if(randomNumber == 3){
     computerMove = 'nożyce';
 }
 printMessage('Mój ruch to: ' + computerMove);
@@ -29,7 +26,33 @@ else if(playerInput == '2'){
 }
 else if(playerInput == '3'){
   playerMove = 'nożyce';
-}
+} 
 
 printMessage('Twój ruch to: ' + playerMove);
-​
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+  printMessage('Komputer wygrywa!');
+}
+else if( computerMove == 'kamień' && playerMove == 'kamień'){
+  printMessage('Remis!');
+}
+else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'nożyce' && playerMove == 'papier'){
+  printMessage('Komputer wygrywa!');
+}
+else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+  printMessage('Remis!');
+}
+else if( computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Komputer wygrywa!');
+}
+else if( computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'papier' && playerMove == 'papier'){
+  printMessage('Remis!');
+}
